f890e1e500c248ece624ec910b29160d1123a3ae
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-20 17:10:45 +0200

    Add formater

--- flake.nix
+++ flake.nix
@@ -5,21 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
     systems = ["x86_64-linux" "aarch64-linux"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
     pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
   in {
-
     devShells = forAllSystems (system: {
       default = pkgs.${system}.mkShell {
         buildInputs = with pkgs.${system}; [
+          # basics
           gitMinimal
+
+          #python
           python3
           pyright
           ruff
           python312Packages.ipykernel
+
+          # nix
+          nil
+          alejandra
         ];
       };
     });
