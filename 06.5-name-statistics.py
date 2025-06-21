64bc510d3c5d2be95a6f347b0d76278fe5b30c98
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-21 22:00:30 +0200

    Read file only once

--- 06.5-name-statistics.py
+++ 06.5-name-statistics.py
@@ -8,39 +8,34 @@ def get_data():
         next(file)  # skip first line
         data = []
         for line in file:
-            column = line.strip().split(",")
-            data.append
+            data.append(line.strip().split(","))
     return data
 
 
-def get_all_years():
-    with open("data/names.csv", "r") as file:
-        next(file)  # skip first line
-        years = []
-        for line in file:
-            column = line.strip().split(",")
-            if not column[2] in years:
-                years.append(column[2])
-        return years
+def get_all_years(data):
+    years = []
+    for line in data:
+        if not line[2] in years:
+            years.append(line[2])
+    return years
 
 
-def get_count_by_name(name, year):
-    with open("data/names.csv", "r") as file:
-        count = 0
-        for line in file:
-            column = line.strip().split(",")
-            if column[1] == name and column[2] == year:
-                count += int(column[5])
-        return count
+def get_count_by_name(data, name, year):
+    count = 0
+    for line in data:
+        if line[1] == name and line[2] == year:
+            count += int(line[5])
+    return count
 
 
 # columns in names.csv are: Id,Name,Year,Gender,State,Count
 def main():
     name = "Anna"
-    years = get_all_years()
+    data = get_data()
+    years = get_all_years(data)
     count = []
     for year in years:
-        countForThisYear = get_count_by_name(name, year)
+        countForThisYear = get_count_by_name(data, name, year)
         print(
             "Found "
             + str(countForThisYear)
