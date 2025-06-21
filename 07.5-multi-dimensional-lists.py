b6132a27f073ade64845259e1aed930e2ca624f6
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-21 23:53:16 +0200

    Combine list and dicts

---
+++ 07.5-multi-dimensional-lists.py
@@ -0,0 +1,8 @@
+list = [["Berlin", "München", "Köln"], ["Budapest", "Pécs", "Sopron"]]
+
+# access first entry
+print(list[0][0])
+
+# combine list and dicts
+students = {"Informatik": ["Max", "Monika"], "BWL": ["Erik", "Franziska"]}
+print(students["Informatik"])
