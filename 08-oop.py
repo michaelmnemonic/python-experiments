52eb0fe2297d94ba1073ee5a48b28352bdbedb3d
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-22 00:08:49 +0200

    Initial experiements with OOP

---
+++ 08-oop.py
@@ -0,0 +1,12 @@
+class Student:
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+
+def main():
+    erik = Student()
+    print()
+
+
+if __name__ == "__main__":
+    main()
