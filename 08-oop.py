c6490c887237eb63da9f6ea604e84871eaba2ba9
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-22 22:40:16 +0200

    "Private" variables

--- 08-oop.py
+++ 08-oop.py
@@ -1,19 +1,28 @@
 class Student:
+    # by convention variables with prefix "_" are private
+    # "__" prefix is enforced by python
     def __init__(self, first_name, last_name):
-        self.firstname = first_name  # assign input values
-        self.last_name = last_name
-        self.tems = 1  # initial value not passed during initialization
+        self.__firstname = first_name  # assign input values
+        self.__last_name = last_name
+        self.__term = 1  # initial value not passed during initialization
 
     def get_full_name(self):
-        return self.first_name + " " + self.last_name
+        self.__first_name + " " + self.__last_name
 
     def increase_term(self):
-        self.term += 1
+        if self.__term >= 9:
+            return  # no student shall have more than 9 terms
+        self.__term += 1
+
+    def get_term(self):
+        return self.__term
 
 
 def main():
     erik = Student("Erik", "Mustermann")
     erik.increase_term()
+    erik.__term = "jkahs"  # does not modify private variable, but much rather "mangles it" -- https://www.geeksforgeeks.org/python/private-variables-python/
+    print(str(erik.get_term))
     print()
 
 
