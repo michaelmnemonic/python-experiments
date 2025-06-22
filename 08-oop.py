fdab0228d22656a0cc142a60af7b14db154e0b92
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-22 22:20:43 +0200

    Add constructor

--- 08-oop.py
+++ 08-oop.py
@@ -1,10 +1,19 @@
 class Student:
+    def __init__(self, first_name, last_name):
+        self.firstname = first_name  # assign input values
+        self.last_name = last_name
+        self.tems = 1  # initial value not passed during initialization
+
     def get_full_name(self):
         return self.first_name + " " + self.last_name
 
+    def increase_term(self):
+        self.term += 1
+
 
 def main():
-    erik = Student()
+    erik = Student("Erik", "Mustermann")
+    erik.increase_term()
     print()
 
 
