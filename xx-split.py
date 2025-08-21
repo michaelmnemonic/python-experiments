98978419073b4f4ee7838410a1a07a256086e0e1
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-21 23:00:25 +0200

    Experiements with generators

---
+++ xx-split.py
@@ -0,0 +1,61 @@
+import os
+import re
+
+def _is_function_start(line):
+    function_name = re.match(r"^ {2}(\w+):", line)
+    if function_name:
+        return function_name.group(1)
+
+def _is_function_comment(line):
+    return re.match(r"^ {2}#.*", line)
+
+
+def _split_functions(lines):
+    current_function = None
+    content = []
+    comments = []
+
+    for line in lines:
+        # Comments are "buffered" until we hit a function start
+        if _is_function_comment(line):
+            comments.append(line)
+            continue
+
+        # Start of new function
+        if _is_function_start(line):
+            if current_function and content:
+                # hand back previous function content
+                yield current_function, content
+                content = []
+
+            current_function = _is_function_start(line)
+
+            # prepend any pending comments
+            if comments:
+                content.extend(comments)
+                comments = []
+
+        # Collect lines for the current function
+        if current_function:
+            content.append(line)
+
+    # Yield last function
+    if current_function and content:
+        yield current_function, content
+
+
+def main() -> None:
+    with open("data/functions.yaml", "r") as file:
+        lines = file.readlines()
+
+    for function_name, content in _split_functions(lines):
+        out_file = f"{function_name}.yaml"
+        with open(out_file, "w") as f:
+            # include the top-level "function:" header for valid YAML
+            f.write("function:\n")
+            f.writelines(content)
+        print(f"Created {out_file}")
+
+
+if __name__ == "__main__":
+    main()
